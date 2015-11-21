7bfcec0831352f2221fb8afd5198956c1d8d3a93
Bryan <bryan@ohgodscience.com>
2015-11-20 23:08:57 -0500

    Modified the views.py and urls.py and template folders with some placeholders.

--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# I'm not sure what test I want.
+# Basically, this should be a bunch of states that we want to test.
+
+# If people don't add in their age, prompt them?
+# If people don't answer all the quesitons, prompt them?
--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 # Create your views here.
+def home(request): #This function is what is returned when survey.views.home is called
+	return render_to_response("survey/home.html", {'hello': "Hello World!"})
