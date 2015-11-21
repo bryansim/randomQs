cac5c5c6ea42f66a3f89baa23ded6d5937143580
Bryan <bryan@ohgodscience.com>
2015-11-21 09:47:38 -0500

    Created two new views: home and questions, and updated urls.py to match. home will eventually be where the questions will be.

--- randomQs/urls.py
+++ randomQs/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^$', 'survey.views.home', name='home'),
+	url(r'^questions/', 'survey.views.questions', name='questions')
 ]
--- survey/views.py
+++ survey/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 def home(request): #This function is what is returned when survey.views.home is called
-	return render_to_response("survey/home.html", {'hello': "Hello World!"})
+	home_text = "Hello, this is the filler text for home.html"
+	return render_to_response("survey/home.html", {'home_text': home_text})
+	
+def questions(request): #This function is what is returned when survey.views.questions is called
+	question_list = [1,2,3,4,5]
+	return render_to_response("survey/questions.html", {'question_list': question_list})
