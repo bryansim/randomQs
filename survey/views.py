c48ee87f5ca4f587920712a62f064e505c20df2d
Bryan <bryan@ohgodscience.com>
2015-11-29 13:12:10 -0500

    Added radio buttons to questions

--- survey/views.py
+++ survey/views.py
@@ -4,9 +4,10 @@ from survey.models import Question
 
 # Create your views here.
 def home(request): #This function is what is returned when survey.views.home is called
-	home_text = "What is your age?" #This eventually becomes Person.person_age
-	return render_to_response("survey/home.html", {'home_text': home_text})
+    home_text = "What is your age?" #This eventually becomes Person.person_age
+    return render_to_response("survey/home.html", {'home_text': home_text})
 	
 def questions(request): #This function is what is returned when survey.views.questions is called
-	question_list = Question.objects.all()
-	return render_to_response("survey/questions.html", {'question_list': question_list})
+    question_list = Question.objects.all()
+    responses = range(1,8)
+    return render_to_response("survey/questions.html", {'question_list': question_list, 'responses' : responses})
